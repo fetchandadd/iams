f9a875f92b184d02b14d13d70bf2575e315acb1e
Tim Tegeler <tim@tegeler.me>
2017-04-23 14:36:08 +0200

    Added hashes endpoint

--- python/src/app.py
+++ python/src/app.py
@@ -6,9 +6,17 @@ import imagehash
 
 class V1(object):
     @staticmethod
-    def generate_hash(hash_function):
+    def read_image():
         body = cherrypy.request.body
-        image = Image.open(body)
+        return Image.open(body)
+
+    @staticmethod
+    def generate_hash(hash_function):
+        image = V1.read_image()
+        return V1.generate_hash_from_image(hash_function, image)
+
+    @staticmethod
+    def generate_hash_from_image(hash_function, image):
         hash_object = hash_function(image)
         return str(hash_object)
 
@@ -19,18 +27,31 @@ class V1(object):
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def perception_hash(self):
-        return {'perception_hash': V1.generate_hash(imagehash.phash)}
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
     def difference_hash(self):
         return {'difference_hash': V1.generate_hash(imagehash.dhash)}
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
+    def perception_hash(self):
+        return {'perception_hash': V1.generate_hash(imagehash.phash)}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
     def wavelet_hash(self):
         return {'wavelet_hash': V1.generate_hash(imagehash.whash)}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def hashes(self):
+        image = V1.read_image()
+        average = V1.generate_hash_from_image(imagehash.average_hash, image)
+        difference = V1.generate_hash_from_image(imagehash.dhash, image)
+        perception = V1.generate_hash_from_image(imagehash.phash, image)
+        wavelet = V1.generate_hash_from_image(imagehash.whash, image)
+        return {'average_hash': average,
+                'difference_hash': difference,
+                'perception_hash': perception,
+                'wavelet_hash': wavelet}
 
 
 if __name__ == '__main__':
