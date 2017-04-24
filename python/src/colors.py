1d65113f1387956d6358dd2233ac6d6e3d18f0e6
Tim Tegeler <tim@tegeler.me>
2017-04-24 18:35:37 +0200

    Using types from typing package

--- python/src/colors.py
+++ python/src/colors.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 import json
 import math
 
@@ -20,11 +21,11 @@ def compute_top_colors_of_image(image, number_of_colors=5):
     return colors[:number_of_colors]
 
 
-def restrict_number_of_colors(colors: list, number_of_colors: int) -> int:
+def restrict_number_of_colors(colors: List, number_of_colors: int) -> int:
     return max(1, min(len(colors), number_of_colors))
 
 
-def normalize_frequency(colors_counted: dict) -> dict:
+def normalize_frequency(colors_counted: Dict) -> Dict:
     base_value = summarize_frequencies(colors_counted)
     for hex_color, frequency in colors_counted.items():
         colors_counted[hex_color] = round(frequency / base_value,
@@ -32,14 +33,14 @@ def normalize_frequency(colors_counted: dict) -> dict:
     return colors_counted
 
 
-def summarize_frequencies(color: dict) -> int:
+def summarize_frequencies(color: Dict) -> int:
     summarized = 0
     for _, frequency in color.items():
         summarized += frequency
     return summarized
 
 
-def build_result_color_list(colors: dict, html_colors: dict) -> list:
+def build_result_color_list(colors: Dict, html_colors: Dict) -> List:
     colors_hex = list(colors.keys())
     return list(map(lambda x: build_item(x,
                                          colors,
@@ -47,34 +48,34 @@ def build_result_color_list(colors: dict, html_colors: dict) -> list:
                     colors_hex))
 
 
-def build_item(hex_color: str, frequencies: dict, html_colors: dict) -> dict:
+def build_item(hex_color: str, frequencies: Dict, html_colors: Dict) -> Dict:
     return {'color': html_colors[hex_color],
             'frequency': frequencies[hex_color]}
 
 
-def get_colors_from_image(image) -> list:
+def get_colors_from_image(image) -> List:
     image.thumbnail(size=thumbnail_size)
     image = image.convert("RGB")
     colors = image.getcolors(maxcolors=max_colors)
     return list(map(convert_frequency_rgb_tuple_to_dict, colors))
 
 
-def load_html_colors() -> dict:
+def load_html_colors() -> Dict:
     with open('./colors.json') as color_file:
         return json.load(color_file)
 
 
-def build_color(color: list) -> dict:
+def build_color(color: List) -> Dict:
     """[0,0,0] -> {'r': 0, 'g': 0, 'b':0}"""
     return {'r': color[0], 'g': color[1], 'b': color[2]}
 
 
-def convert_frequency_rgb_tuple_to_dict(frequency_rgb_tuple: list) -> dict:
+def convert_frequency_rgb_tuple_to_dict(frequency_rgb_tuple: List) -> Dict:
     return {'frequency': frequency_rgb_tuple[0],
             'rgb': build_color(frequency_rgb_tuple[1])}
 
 
-def count_color_classes(colors: list) -> dict:
+def count_color_classes(colors: List[Dict]) -> Dict:
     color_dict = {}
     for color in colors:
         try:
@@ -84,39 +85,39 @@ def count_color_classes(colors: list) -> dict:
     return color_dict
 
 
-def sort_colors_by_frequency(colors: list) -> list:
+def sort_colors_by_frequency(colors: List) -> List:
     return sorted(colors,
                   key=lambda color: color['frequency'],
                   reverse=True)
 
 
-def euclid_distance(x: dict, y: dict) -> float:
+def euclid_distance(x: Dict, y: Dict) -> float:
     """{'r':2, 'g':1, 'b':3}, {'r':0, 'g':0, 'b':0} -> 3.7416573868"""
     return math.sqrt((x['r'] - y['r']) ** 2 +
                      (x['g'] - y['g']) ** 2 +
                      (x['b'] - y['b']) ** 2)
 
 
-def classify_color_list(colors: list, html_color_values: list) -> list:
+def classify_color_list(colors: List, html_color_values: List) -> List:
     return list(map(lambda x: classify_color(x, html_color_values), colors))
 
 
-def classify_color(frequency_rgb_dict: dict, html_color_values: list) -> dict:
+def classify_color(frequency_rgb_dict: Dict, html_color_values: List) -> Dict:
     color_class = get_color_class(frequency_rgb_dict['rgb'],
                                   html_color_values)
     return {'frequency': frequency_rgb_dict['frequency'],
             'color_class': color_class}
 
 
-def get_color_class(color: dict, html_color_values: list) -> int:
+def get_color_class(color: Dict, html_color_values: List) -> int:
     """{'r': 0, 'g': 0, 'b':0} -> 000000"""
     color_distance_list = distance_to_color_class(color, html_color_values)
     closest_color = min(color_distance_list, key=lambda x: x['distance'])
     return closest_color['hex']
 
 
-def distance_to_color_class(target_color: dict,
-                            html_color_values: list) -> list:
+def distance_to_color_class(target_color: Dict,
+                            html_color_values: List) -> List:
     return list(map(lambda x: {'hex': x['hex'],
                                'distance': euclid_distance(x,
                                                            target_color)},
