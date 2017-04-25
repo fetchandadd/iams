9abbba8eb65f0dd1c1e2c0555179049f53e83fb7
Tim Tegeler <tim@tegeler.me>
2017-04-25 09:51:01 +0200

    Added test for normalize_frequency

--- python/src/test_colors.py
+++ python/src/test_colors.py
@@ -1,9 +1,13 @@
 import unittest
 
-from colors import summarize_frequencies, count_color_classes
+from colors import summarize_frequencies, count_color_classes, normalize_frequency
 
 
 class TestColorsFunction(unittest.TestCase):
+    @staticmethod
+    def fake_map(iterables):
+        return map(lambda x: x, iterables)
+
     def test_summarize_frequencies(self):
         self.assertEqual(summarize_frequencies({'000000': 1}), 1)
         self.assertEqual(summarize_frequencies({'000000': 1,
@@ -13,7 +17,7 @@ class TestColorsFunction(unittest.TestCase):
                                                 'FFFFFF': 3}), 6)
 
     def test_count_color_classes(self):
-        test_input = [
+        test_input = TestColorsFunction.fake_map([
             {
                 'frequency': 5,
                 'color_class': '000000'
@@ -26,9 +30,14 @@ class TestColorsFunction(unittest.TestCase):
                 'frequency': 10,
                 'color_class': 'FFFFFF'
             },
-        ]
+        ])
         expected_output = {'000000': 9, 'FFFFFF': 10}
         self.assertEqual(count_color_classes(test_input), expected_output)
+
+    def test_normalize_frequency(self):
+        test_input = {'000000': 4, 'FFFFFF': 6}
+        expected_output = {'000000': 0.4, 'FFFFFF': 0.6}
+        self.assertEqual(normalize_frequency(test_input), expected_output)
 
 
 if __name__ == '__main__':
